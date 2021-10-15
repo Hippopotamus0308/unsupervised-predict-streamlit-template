f13db186e8bca91c1dd29b4d70e2c60557457515
Hippopotamus0308 <louisyang0308@163.com>
2021-10-15 16:21:16 +0800

    chore: streamlit file

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -1,36 +1,4 @@
-"""
-
-    Streamlit webserver-based Recommender Engine.
-
-    Author: Explore Data Science Academy.
-
-    Note:
-    ---------------------------------------------------------------------
-    Please follow the instructions provided within the README.md file
-    located within the root of this repository for guidance on how to use
-    this script correctly.
-
-    NB: !! Do not remove/modify the code delimited by dashes !!
-
-    This application is intended to be partly marked in an automated manner.
-    Altering delimited code may result in a mark of 0.
-    ---------------------------------------------------------------------
-
-    Description: This file is used to launch a minimal streamlit web
-	application. You are expected to extend certain aspects of this script
-    and its dependencies as part of your predict project.
-
-	For further help with the Streamlit framework, see:
-
-	https://docs.streamlit.io/en/latest/
-
-"""
-# Streamlit dependencies
 import streamlit as st
-
-# Data handling dependencies
-import pandas as pd
-import numpy as np
 
 # Custom Libraries
 from utils.data_loader import load_movie_titles
@@ -39,42 +7,43 @@ from recommenders.content_based import content_model
 
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
+DEFAULT_ENDPOINT = 'http://127.0.0.1:45678/search'
+TOP_K = 10
+
 
 # App declaration
 def main():
-
-    # DO NOT REMOVE the 'Recommender System' option below, however,
-    # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
-
-    # -------------------------------------------------------------------
-    # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
-    # -------------------------------------------------------------------
+    page_options = ["Movie Recommender System","Other RS"]
     page_selection = st.sidebar.selectbox("Choose Option", page_options)
-    if page_selection == "Recommender System":
-        # Header contents
-        st.write('# Movie Recommender Engine')
-        st.write('### EXPLORE Data Science Academy Unsupervised Predict')
-        st.image('resources/imgs/Image_header.png',use_column_width=True)
-        # Recommender System algorithm selection
-        sys = st.radio("Select an algorithm",
+    st.write('# Recommendation System')
+    if page_selection == "Movie Recommender System":
+        # Sider contents
+        sys = st.sidebar.radio("Select an algorithm",
                        ('Content Based Filtering',
                         'Collaborative Based Filtering'))
+        settings = st.sidebar.expander(label='Settings', expanded=False)
+        with settings:
+            endpoint = st.text_input(label='Endpoint', value=DEFAULT_ENDPOINT)
+            top_k = st.number_input(label='Top K', value=TOP_K, step=1)
+
+        # Header contents
+        st.write('## Movie Recommender Engine')
+        st.image('resources/imgs/Image_header.png',use_column_width=True)
 
         # User-based preferences
         st.write('### Enter Your Three Favorite Movies')
-        movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
-        movie_2 = st.selectbox('Second Option',title_list[25055:25255])
-        movie_3 = st.selectbox('Third Option',title_list[21100:21200])
+        movie_1 = st.selectbox('First Option',title_list)
+        movie_2 = st.selectbox('Second Option',title_list)
+        movie_3 = st.selectbox('Third Option',title_list)
         fav_movies = [movie_1,movie_2,movie_3]
-
-        # Perform top-10 movie recommendation generation
+        fav_movie = ['hippoman1', 'hippoman2', 'hippoman3']
+        # Perform top-n movie recommendation generation
         if sys == 'Content Based Filtering':
             if st.button("Recommend"):
                 try:
                     with st.spinner('Crunching the numbers...'):
                         top_recommendations = content_model(movie_list=fav_movies,
-                                                            top_n=10)
+                                                            top_n=top_k)
                     st.title("We think you'll like:")
                     for i,j in enumerate(top_recommendations):
                         st.subheader(str(i+1)+'. '+j)
@@ -96,16 +65,9 @@ def main():
                     st.error("Oops! Looks like this algorithm does't work.\
                               We'll need to fix it!")
 
-
-    # -------------------------------------------------------------------
-
-    # ------------- SAFE FOR ALTERING/EXTENSION -------------------
-    if page_selection == "Solution Overview":
-        st.title("Solution Overview")
-        st.write("Describe your winning approach on this page")
-
-    # You may want to add more sections here for aspects such as an EDA,
-    # or to provide your business pitch.
+    if page_selection == "Other RS":
+        st.title("Other RS")
+        st.write("Wait to add.")
 
 
 if __name__ == '__main__':
