fb7048471bc1a9cb003895b10b987fb426ac8f6b
Hippopotamus0308 <louisyang0308@163.com>
2021-10-15 17:46:59 +0800

    feat: change search type

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -1,56 +1,69 @@
 import streamlit as st
 
 # Custom Libraries
-from utils.data_loader import load_movie_titles
+from utils.data_loader import load_movie_titles,load_movie_genres
 from recommenders.collaborative_based import collab_model
 from recommenders.content_based import content_model
 
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
+genre = load_movie_genres('resources/data/movies.csv')
 DEFAULT_ENDPOINT = 'http://127.0.0.1:45678/search'
 TOP_K = 10
 
 
 # App declaration
 def main():
-    page_options = ["Movie Recommender System","Other RS"]
-    page_selection = st.sidebar.selectbox("Choose Option", page_options)
+    recommend_options = ["Movie Recommender System","Other RS"]
+    page_selection = st.sidebar.selectbox("Choose Option", recommend_options)
+    type_choice = st.sidebar.selectbox(
+        'Please choose the type of your input:',
+        [{'name': 'search_by_genre', 'display': 'Recommend from favored genres.'},
+         {'name': 'search_by_object', 'display': 'Recommend from favored objects.'}],
+        format_func=lambda x: x['display']
+    )['name']
     st.write('# Recommendation System')
+    # Sider contents
+    sys = st.sidebar.radio("Select an algorithm",
+                           ('Content Based Filtering',
+                            'Collaborative Based Filtering'))
+    settings = st.sidebar.expander(label='Settings', expanded=False)
+    with settings:
+        endpoint = st.text_input(label='Endpoint', value=DEFAULT_ENDPOINT)
+        top_k = st.number_input(label='Top K', value=TOP_K, step=1)
+
     if page_selection == "Movie Recommender System":
-        # Sider contents
-        sys = st.sidebar.radio("Select an algorithm",
-                       ('Content Based Filtering',
-                        'Collaborative Based Filtering'))
-        settings = st.sidebar.expander(label='Settings', expanded=False)
-        with settings:
-            endpoint = st.text_input(label='Endpoint', value=DEFAULT_ENDPOINT)
-            top_k = st.number_input(label='Top K', value=TOP_K, step=1)
 
         # Header contents
         st.write('## Movie Recommender Engine')
         st.image('resources/imgs/Image_header.png',use_column_width=True)
 
-        # User-based preferences
-        st.write('### Enter Your Three Favorite Movies')
-        movie_1 = st.selectbox('First Option',title_list)
-        movie_2 = st.selectbox('Second Option',title_list)
-        movie_3 = st.selectbox('Third Option',title_list)
-        fav_movies = [movie_1,movie_2,movie_3]
-        fav_movie = ['hippoman1', 'hippoman2', 'hippoman3']
+        if type_choice == "search_by_object":
+            # User-based preferences
+            st.write('### Enter Your Three Favorite Movies')
+            movie_1 = st.selectbox('First Option',title_list)
+            movie_2 = st.selectbox('Second Option',title_list)
+            movie_3 = st.selectbox('Third Option',title_list)
+            fav_movies = [movie_1, movie_2, movie_3]
+        else:
+            st.write('### Choose genres you prefer:')
+            container1 = st.container()
+            sectors = container1.multiselect(" ",genre,
+                                             help="You can directly type words to search and choose genres.")
+
         # Perform top-n movie recommendation generation
         if sys == 'Content Based Filtering':
             if st.button("Recommend"):
                 try:
                     with st.spinner('Crunching the numbers...'):
                         top_recommendations = content_model(movie_list=fav_movies,
-                                                            top_n=top_k)
+                                                            top_n=10)
                     st.title("We think you'll like:")
                     for i,j in enumerate(top_recommendations):
                         st.subheader(str(i+1)+'. '+j)
                 except:
                     st.error("Oops! Looks like this algorithm does't work.\
                               We'll need to fix it!")
-
 
         if sys == 'Collaborative Based Filtering':
             if st.button("Recommend"):
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -28,3 +28,18 @@ def load_movie_titles(path_to_movies):
     df = df.dropna()
     movie_list = df['title'].to_list()
     return movie_list
+
+
+def load_movie_genres(path_to_movies):
+    df = pd.read_csv(path_to_movies)
+    df = df.dropna()
+    movie_list = df['genres'].dropna().str.split("|").to_list()
+    new_list = []
+    for item in movie_list:
+        if item:
+            for i in item:
+                if i != '(no genres listed)':
+                    new_list.append(i)
+    new_list = list(set(new_list))
+
+    return new_list
